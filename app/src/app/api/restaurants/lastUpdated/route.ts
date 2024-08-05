95498e8e5055af599a776177b9e653e2162b598a
Rodrigo Neves <rneves.cse@gmail.com>
2024-08-06 00:52:22 +0100

    Force GET lastUpdated to not be cached

--- app/src/app/api/restaurants/lastUpdated/route.ts
+++ app/src/app/api/restaurants/lastUpdated/route.ts
@@ -1,6 +1,8 @@
 import { RestaurantsService, RestaurantsImpl } from "@/lib/places/service";
 import { NotionAPIRestaurantsRepository } from "@/lib/places/repository/notion-api";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const restaurantService: RestaurantsService = 
 		new RestaurantsImpl(new NotionAPIRestaurantsRepository());
--- app/src/app/api/restaurants/route.ts
+++ app/src/app/api/restaurants/route.ts
@@ -1,8 +1,7 @@
 import { RestaurantsService, RestaurantsImpl } from "@/lib/places/service";
 import { NotionAPIRestaurantsRepository } from "@/lib/places/repository/notion-api";
-import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
 	const repoImpl = new NotionAPIRestaurantsRepository();
     const restaurantService: RestaurantsService = new RestaurantsImpl(repoImpl);
 
