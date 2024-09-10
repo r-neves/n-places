1119efe5c89c7536dce65833d55400ed91db0227
Rodrigo Neves <rneves.cse@gmail.com>
2024-09-10 01:26:40 +0100

    fix: Import Analytics and SpeedInsights vercel features

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,29 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "N Places",
-	description: "See your saved places on the map!",
+    title: "N Places",
+    description: "See your saved places on the map!",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<head>
-				<link rel="manifest" href="/manifest.json" />
-				<link
-					rel="stylesheet"
-					href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css"
-				/>
-			</head>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <head>
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css"
+                />
+            </head>
+            <body className={inter.className}>
+                {children}
+                <Analytics />
+				<SpeedInsights />
+            </body>
+        </html>
+    );
 }
