b07aa3a4c5720689c3a4c02320fc3f8a74704b5f
Rodrigo Neves <rneves.cse@gmail.com>
2025-05-25 00:17:29 +0100

    Test green screen

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,8 +4,7 @@ import MapComponent from "../components/Map";
 export default async function Home() {
     return (
         <main className={styles.main}>
-            <h1 className={styles.title}>Welcome to the Map App</h1>
-            <MapComponent/>
+            <h3 className={styles.title}>Welcome to the Map App</h3>
         </main>
     );
 }
