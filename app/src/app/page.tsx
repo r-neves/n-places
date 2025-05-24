ecf3f12d5baf621afa50b3a20f59ec134e87d026
Rodrigo Neves <rneves.cse@gmail.com>
2025-05-25 00:14:45 +0100

    Test padding in every component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,6 +4,7 @@ import MapComponent from "../components/Map";
 export default async function Home() {
     return (
         <main className={styles.main}>
+            <h1 className={styles.title}>Welcome to the Map App</h1>
             <MapComponent/>
         </main>
     );
