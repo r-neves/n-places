cb9d038869f5450941a0eaa2d1a58fef1e68b4ea
Rodrigo Neves <rneves.cse@gmail.com>
2025-05-25 13:43:03 +0100

    Test mapCanvas top -20px

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,7 +4,7 @@ import MapComponent from "../components/Map";
 export default async function Home() {
     return (
         <main className={styles.main}>
-            <h3 className={styles.title}>Welcome to the Map App</h3>
+            <MapComponent/>
         </main>
     );
 }
