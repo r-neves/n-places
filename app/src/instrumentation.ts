d93a5d11a22bde617d2ed591158c9c7c283d016d
Rodrigo Neves <rneves@cynergybank.co.uk>
2025-08-16 15:58:03 +0100

    fix: instrumentation TS syntax

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -1,5 +1,4 @@
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { registerOTel } from '@vercel/otel';
 
 export function register() {
@@ -11,16 +10,12 @@ export function register() {
 
     registerOTel({
         serviceName: 'nextjs-app',
-        spanProcessors: [
-            new SimpleSpanProcessor(
-                new OTLPTraceExporter({
-                    url: `https://${process.env.AXIOM_DOMAIN}/v1/traces`,
-                    headers: {
-                        Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
-                        'X-Axiom-Dataset': `${process.env.AXIOM_DATASET_NAME}`,
-                    },
-                })
-            )
-        ],
+        traceExporter: new OTLPTraceExporter({
+            url: `https://${process.env.AXIOM_DOMAIN}/v1/traces`,
+            headers: {
+                Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
+                'X-Axiom-Dataset': `${process.env.AXIOM_DATASET_NAME}`,
+            },
+        }),
     });
 }
